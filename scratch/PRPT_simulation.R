ecf2e1c1d5d447ee63dd13d06df5d9e102340c08
Jonathan Trattner <accounts@jdtrat.com>
2020-08-04 15:07:12 -0400

    Commented PRPT script with Paul. (Barely) started splitting it into subcomponents.

--- scratch/PRPT_simulation.R
+++ scratch/PRPT_simulation.R
@@ -1,27 +1,63 @@
+
+setup
+
+
+
+
+
+
+
+simulatePRPT <- function(numIters = 1000, numTrials = 150) {
+
+}
+
+
   numiters = 1000 #number of iterations
   numtrials = 150 #number of trials
 
+  #Initialize vectors for total earnings accrued on each simulation
+  # 100 by 1 vector
   Random_earnings = matrix(nrow = numiters, ncol = 1)
+  #Percent correct for each iteration -- overall percent correct
   Random_correctness = matrix(nrow = numiters, ncol = 1)
+  #Running average of the percent correct
+  #list of ones and zeros for whether they were correct on the trial
   Random_correctness_run = matrix(nrow = numiters, ncol = numtrials)
 
 
   for (k in 1:numiters) {
 
-  nEpisode = 4
-  reward_episode = matrix(nrow = 4, ncol = numtrials)
+  nEpisode = 4 #initialize number of states
+  #reward occuring at each state
+  #number of rows is the amount of states by number of trials
+  reward_episode = matrix(nrow = nEpisode, ncol = numtrials)
+  # initialize choice vector which stores the two options (fractal)
+  # images on each trial -- 1 to 6.
   choice = matrix(nrow = numtrials, ncol = 2)
+  # decision is number 1 - 6 corresponding to icon group.
+  # this is the intitialization of the decision matrix
   decision = matrix(nrow = 1, ncol = numtrials)
+  #initialize percent corrrect
   percent_correct = matrix(nrow = 1, ncol = numtrials)
 
+  # make a vector of 100 elements
   Reward_vector = matrix(nrow = 6, ncol = 100)
+  # what values of rewards are possible for each action
+  # column 1 is phase 1 and 2 and column 2 is phase 3
   Reward_structure = matrix(nrow = 6, ncol = 2)
+  # filling in the reward vector matrix which has the
+  # probabilities of getting a reward.
+  # for instance reward_vector[1,] is 75 0s and 25 ones, corresponding to
+  # 25% rpobability of getting reward
   Reward_vector[1,] <- c(rep(0,75),rep(1,25))
   Reward_vector[2,] <- c(rep(0,50),rep(1,50))
   Reward_vector[3,] <- c(rep(0,25),rep(1,75))
   Reward_vector[4,] <- c(rep(0,75),rep(1,25))
   Reward_vector[5,] <- c(rep(0,50),rep(1,50))
   Reward_vector[6,] <- c(rep(0,25),rep(1,75))
+  #Reward structure is the magnitude of the rewards
+  #column 1 is for phase 1 and 2 values
+  #column 2 is for phase 3 values
   Reward_structure[1,] <- c(1,2.5)
   Reward_structure[2,] <- c(1,1.5)
   Reward_structure[3,] <- c(1,0.5)
@@ -29,18 +65,28 @@
   Reward_structure[5,] <- c(-1,-0.75)
   Reward_structure[6,] <- c(-1,-0.25)
 
+  # start to define which choices will be on each trial
+  # this only corresponds to phase 2
+  # 50 trials, you pull 25 positive
+  # and 25 negatives
+  #
   Choice_vector = c(rep(0,25), rep(1,25))
+  # sample without replacement and say which order of trials
+  # will you have positive or negative trials (icons) in phase 2
   Choice_vector_shuffled = sample(Choice_vector,50)
 
-
+  #for each trial,
   for (i in 1:numtrials) {
 
       # Phase One choices
       if (i < 25 || i == 25) {
+        # sample 2 icons corresponding to the group images to show on a trial
             choice[i,] <- sample(c(1,2,3),2)}
 
       # Phase Two choices
       if (i > 25 && i <=75) {
+        # sample 2 icons corresponding to the group images to show on a trial
+        # if it's a 1 it is a positive icon, if it's a 0 it's a negative icon
             if (Choice_vector_shuffled[i-25] == 0)
                     choice[i,] <- sample(c(1,2,3),2)
             if (Choice_vector_shuffled[i-25] == 1)
@@ -48,6 +94,8 @@
 
       # Phase 3 choices
       if (i > 75) {
+        # sample 2 icons corresponding to the group images to show on a trial
+        # both positive and negative
               choice[i,] <- sample(c(1,2,3,4,5,6),2)}
   }
 
@@ -55,19 +103,20 @@
   for (i in 1:numtrials) {
 
       # Create variable ('Decision_vector') with 50/50 chance of choosing choice
-      # 1 or choice 2, shuffle 'Decision_vector' x1000, and make decision
-      # according to first element of shuffled 'Decision_vector'
-      Decision_vector <- c(rep(choice[i,1],50), rep(choice[i,2],50))
+      # 1 or choice 2 and shuffle it and draw the first element
+      Decision_vector <- c(rep(choice[i,1],50), rep(choice[i,2],50)) # For softmax these 50s should become the calculated softmax probabilities.
       Decision_vector_shuffled <- sample(Decision_vector,100)
-      decision[1,i] <- Decision_vector_shuffled[1]
+      decision[1,i] <- Decision_vector_shuffled[1] # save decision to the decision matrix
 
 
       # Determine whether reward is obtained or not. If the first element of
       # 'Reward_vector_shuffled'=1, then reward is obtained. The reward
       # magnitude depends on the trial # (different reward magnitudes by task
       # Phase).
-      Reward_vector_trial <- Reward_vector[decision[1,i],]
-      Reward_vector_shuffled <- sample(Reward_vector_trial,100)
+      Reward_vector_trial <- Reward_vector[decision[1,i],] # select the row that has the definitions of whether or not a trial was rewarded
+      Reward_vector_shuffled <- sample(Reward_vector_trial,100) # sample that without replacement. If first element is 1, get rewarded. Else no reward.
+
+      #in phase 1 and 2 add reward if appropriate
       if (i < 76) {
           if (Reward_vector_shuffled[1] == 1) {
               reward_episode[3,i] <- Reward_structure[decision[1,i],1]}
@@ -75,6 +124,7 @@
               reward_episode[3,i] <- 0 }
       }
 
+      # in phase 3 add reward if appropriate (second column phase 3 values)
       if (i > 75) {
           if (Reward_vector_shuffled[1] == 1) {
               reward_episode[3,i] <- Reward_structure[decision[1,i],2]}
